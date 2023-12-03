1353f8659fa703587068404329089a9a521af430
kaique-psantos <kaique212pereira@gmail.com>
2023-12-03 02:06:23 -0300

    removido credenciais do banco de dados por seguranaça

--- src/br/com/projectgerir/connection/ConnectionFactory.java
+++ src/br/com/projectgerir/connection/ConnectionFactory.java
@@ -13,9 +13,9 @@ import javax.swing.JOptionPane;
  */
 public class ConnectionFactory {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://aws.connect.psdb.cloud/dbgerir?sslMode=VERIFY_IDENTITY";
-    private static final String USER = "fk0fwp8qde8vyx576h73";
-    private static final String PASS = "pscale_pw_jZC1LndmW9wk01RoT13nKFNGfedkttPmxhPbUgimnaL";
+    private static final String URL = "REMOVIDO POR SEGURANÇA";
+    private static final String USER = "REMOVIDO POR SEGURANÇA";
+    private static final String PASS = "REMOVIDO POR SEGURANÇA";
     
     public static Connection getConnection(){
         try {
